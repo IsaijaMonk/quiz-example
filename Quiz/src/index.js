2963266b451ce47e3cc93dbabb3f965597c1710d
IsaijaMonk <isaijamonk@gmail.com>
2023-05-04 15:37:55 +0500

    new project

---
+++ Quiz/src/index.js
@@ -0,0 +1,9 @@
+const previousStepBtn = document.querySelector('.previous-step-btn');
+previousStepBtn.addEventListener('click', () => {
+  console.log("Назад");
+});
+
+const nextStepBtn = document.querySelector('.next-step-btn');
+nextStepBtn.addEventListener('click', () => {
+  console.log("Вперед");
+});
